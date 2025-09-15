8cb21da44a1be391e12397bf4585d14cbf3d2fc4
Brandon J <bsjulao@gmail.com>
2025-09-15 13:59:58 -0700

    Commit - Headers and prototypes

--- lab_6_dynamicarraysfunctions.cpp
+++ lab_6_dynamicarraysfunctions.cpp
@@ -1,1 +1,7 @@
-// Test Commit
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void enterArrayData(double* arr, size_t size);
+void outputArrayData(const double* arr, size_t size);
+double sumArray(const double* arr, size_t size);
